5787dcc2a9362a74b5a2cf665a961e7bb29f52e7
Henri Cavalheiro <hcavalheiroavanco@gmail.com>
2024-05-13 13:42:25 -0300

    Update app.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import Cadastro from './pages/Cadastro.jsx'
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Login from "./pages/Login";
-import CadastroTurmas from "./pages/CadastroTurmas";
+import EdicaoPerfil from "./pages/EdicaoPerfil";
 
 function App() {
   return (
       <BrowserRouter>
           <Routes>
-              <Route path="/cadastroTurmas" element={<CadastroTurmas  />}/>
               <Route path="/login" element={<Login />}/>
               <Route path="/cadastro" element={<Cadastro />}/>
+              <Route path="/edicao" element={<EdicaoPerfil/>}/>
           </Routes>
       </BrowserRouter>
   );
