d4f4ad1d428f3c597e5d7d12b12b85a376fa83ea
Soares363 <124930916+Soares363@users.noreply.github.com>
2024-05-21 09:24:54 -0300

    BarraLateral.jsx

--- src/components/BarraLateral.jsx
+++ src/components/BarraLateral.jsx
@@ -1,6 +1,36 @@
 import css from "./BarraLateral.module.css"
-
+import { useNavigate } from 'react-router-dom';
 export default function BarraLateral(){
+    const navigate = useNavigate();
+
+    const irPara1 = () => {
+        navigate("/salasCadastradas");
+    };
+
+    const irPara2 = () => {
+        navigate("/cursos");
+    };
+
+    const irPara3 = () => {
+        navigate("/Turmas");
+    };
+
+    const irPara4 = () => {
+        navigate("/calendario");
+    };
+
+    const irPara5 = () => {
+        navigate("/usuarios");
+    };
+
+    const irPara6 = () => {
+        navigate("/edicao");
+    };
+
+
+
+
+
     return (
         <div className={css.barra + ' w-100'}>
             <div className={css.logoBarra}>
@@ -15,28 +45,28 @@ export default function BarraLateral(){
                     </div>
                 </div>
             </div>
-            <button className={css.divI}>
+            <button onClick={irPara1} className={css.divI}>
                 <img src="./sala.svg" alt=""/>
                 <p>Cadastro de Salas</p>
             </button>
-            <button className={css.divI}>
+            <button onClick={irPara2} className={css.divI}>
                 <img src="./Livro.svg" alt=""/>
                 <p>Cadastro de Curso</p>
             </button>
-            <button className={css.divI}>
+            <button onClick={irPara3} className={css.divI}>
                 <img src="./certificado.svg" alt=""/>
                 <p>Cadastro de Turmas</p>
             </button>
-            <button className={css.divI}>
+            <button onClick={irPara4} className={css.divI}>
                 <img src="./calendario.svg" alt=""/>
                 <p>Calendário Anual</p>
             </button>
-            <button className={css.divI}>
+            <button onClick={irPara5} className={css.divI}>
                 <img src="./userMais.svg" alt=""/>
                 <p>Cadastro de Usuarios</p>
             </button>
 
-            <button className={css.divII}>
+            <button onClick={irPara6} className={css.divII}>
                 <img src="./Config.png" alt=""/>
                 <p>Configurações</p>
             </button>
