0211ea0fb348af35b82da3bbac17b2fb5d920127
Soares363 <124930916+Soares363@users.noreply.github.com>
2024-05-28 13:36:19 -0300

    CadastrarTurmas.jsx

--- src/pages/CadastrarTurmas.jsx
+++ src/pages/CadastrarTurmas.jsx
@@ -3,10 +3,13 @@ import BarraLateral from "../components/BarraLateral";
 import BotaoCriar from "../components/BotaoCriar";
 import InputSearch from "../components/InputSearch";
 import SelecAluno from "../components/SelecAluno";
-import DaySelector from "../components/SeletorDeDias";
 import SeletorDeDias from "../components/SeletorDeDias";
+import ComboBoxExample from "../components/ComboBox";
+import {useState} from "react";
+
 
 export default function CadastrarTurmas() {
+    const [sala, setSala] = useState(null)
     return (
         <div className={css.main + ' container-fluid'}>
             <div className={css.tudo + " row"}>
@@ -24,18 +27,68 @@ export default function CadastrarTurmas() {
                         <div className={css.divtudo + ' row'}>
                             <div className={css.divcadastro + ' col-8'}>
                                 <div className={css.divinputs + ' row w-100 '}>
-                                    <input placeholder={'Nome'} className={css.inputs + ' col-12'} type="text"/>
-                                    <input placeholder={'Início'} className={css.inputs + ' col-4'} type="text"/>
-                                    <input placeholder={'Término'} className={css.inputs + ' col-4'} type="text"/>
-                                    <input placeholder={'Horário'} className={css.inputs + ' col-4'} type="text"/>
-                                    <input placeholder={'Docente/Professor'} className={css.inputs + ' col-8'} type="text"/>
-                                    <input placeholder={'Sala'} className={css.inputs + ' col-4'} type="text"/>
+                                    <input placeholder={'Nome Da turma'} className={css.inputs + ' col-12'} type="text"/>
+
+                                    <div className={css.juncaoCB}>
+                                        <input placeholder={'Início'} className={css.inputs + ' col-4'} type="date"/>
+                                        <input placeholder={'Término'} className={css.inputs + ' col-4'} type="date"/>
+                                        <ComboBoxExample
+                                            label={" "}
+                                            titulo={'Salas'}
+                                            tamanho={'10vw'}
+                                            setValor={setSala}
+                                            itens={[
+                                                {
+                                                    nome: "Sala 22",
+                                                    valor: 1
+                                                },
+                                                {
+                                                    nome: "Sala 23",
+                                                    valor: 2
+                                                }
+                                            ]}
+                                        />
+                                    </div>
+
+
+                                    <ComboBoxExample
+                                        label={" "}
+                                        tamanho={'38vw'}
+                                        titulo={'Professores/Instrutores'}
+                                        setValor={setSala}
+                                        itens={[
+                                            {
+                                                nome: "Igor",
+                                                valor: 1
+                                            },
+                                            {
+                                                nome: "Lais",
+                                                valor: 2
+                                            }
+                                        ]}
+                                    />
+
+
                                 </div>
                                 <p>Aulas Semanais:</p>
                                 <SeletorDeDias></SeletorDeDias>
-                                <textarea className={ css.textarea}>
 
-                                </textarea>
+                                <ComboBoxExample
+                                    label={" "}
+                                    tamanho={'38vw'}
+                                    titulo={'Selecionar curso'}
+                                    setValor={setSala}
+                                    itens={[
+                                        {
+                                            nome: "TI",
+                                            valor: 1
+                                        },
+                                        {
+                                            nome: "Eletromecânica",
+                                            valor: 2
+                                        }
+                                    ]}
+                                />
                             </div>
                             <div className={css.divalunos + ' col-4'}>
                                 <div className={css.alunos}>
@@ -64,4 +117,5 @@ export default function CadastrarTurmas() {
                 </div>
             </div>
         </div>
-    );}
+
+    )}
